c2e7724fe0f5e6032d8b5a563dc255d112d087ad
kinskin <md.asshikin@linqcash.com>
2019-10-31 15:26:27 +0800

    hello

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 console.log("hello script js");
 /*
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
-};
-*/
 
 //create game that guess a secret word
 //secret word = cat
